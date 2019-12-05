e64dae87bbfe6cf730a8bade03c16535573eee01
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2019-12-05 19:41:46 +0100

    Remove FileManger

--- src/FileManager.java
+++ src/FileManager.java
@@ -13,44 +13,4 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
 public class FileManager
-{
-   private String[] fileNames;
-   private File[] files;
-
-   public FileManager()
-
-  {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    try
-    {
-      DocumentBuilder builder = factory.newDocumentBuilder();
-      Document doc = builder.newDocument();
-
-      Element rootElement = doc.createElement("GradeList");
-      for (int i = 0; i < grades.size(); i++)
-      {
-        Element subElement = doc.createElement("Grade");
-        Element subSubElement = doc.createElement("Course");
-        subSubElement.appendChild(doc.createTextNode(grades.getGrade(i).getCourse()));
-        subElement.appendChild(subSubElement);
-        subSubElement = doc.createElement("Grade");
-        subSubElement.appendChild(doc.createTextNode(
-            Integer.toString(grades.getGrade(i).getGrade())));
-        subElement.appendChild(subSubElement);
-        rootElement.appendChild(subElement);
-      }
-      doc.appendChild(rootElement);
-
-      Transformer transformer = TransformerFactory.newInstance().newTransformer();
-      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-      transformer
-          .setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-      transformer.transform(new DOMSource(doc), new StreamResult(file));
-
-    }
-    catch (ParserConfigurationException | TransformerException e)
-    {
-      System.out.println("Error");
-    }
-  }
-}
+{}
