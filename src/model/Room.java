6d51f7fb5a62a73ab9f9b2cbadfd364a58508062
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2019-12-06 11:10:33 +0100

    room update

--- src/model/Room.java
+++ src/model/Room.java
@@ -5,43 +5,82 @@ import java.io.Serializable;
 public class Room implements Serializable
 {
   private String roomName;
-  private boolean hasProjector;
+  private String typesOfConnectorsAvailable;
   private int maxNumberOfStudents;
+  private int numberOfChairs;
+  private int numberOfTables;
+  private boolean canBeMerged;
 
-
-  public Room(String roomName, boolean isThereProjector, int maxNumberOfStudents)
+  public Room(String roomName, String typesOfConnectorsAvailable,
+      int maxNumberOfStudents, int numberOfChairs, int numberOfTables,
+      boolean canBeMerged)
   {
-     this.roomName = roomName;
-     this.hasProjector = isThereProjector;
-     this.maxNumberOfStudents = maxNumberOfStudents;
+    this.roomName = roomName;
+    this.typesOfConnectorsAvailable = typesOfConnectorsAvailable;
+    this.maxNumberOfStudents = maxNumberOfStudents;
+    this.numberOfChairs = numberOfChairs;
+    this.numberOfTables = numberOfTables;
+    this.canBeMerged = canBeMerged;
   }
-
 
   public String getRoomName()
   {
     return roomName;
   }
-  public boolean isThereAProjector()
+
+  public String getTypesOfConnectorsAvailable()
   {
-    return hasProjector;
+    return typesOfConnectorsAvailable;
   }
+
   public int getMaxNumberOfStudents()
   {
     return maxNumberOfStudents;
   }
 
+  public int getNumberOfChairs()
+  {
+    return numberOfChairs;
+  }
+
+  public int getNumberOfTables()
+  {
+    return numberOfTables;
+  }
+
+  public boolean isCanBeMerged()
+  {
+    return canBeMerged;
+  }
 
   public void setRoomName(String roomName)
   {
     this.roomName = roomName;
   }
-  public void setHasProjector(Boolean hasProjector)
+
+  public void setTypesOfConnectorsAvailable(String typesOfConnectorsAvailable)
   {
-    this.hasProjector = hasProjector;
+    this.typesOfConnectorsAvailable = typesOfConnectorsAvailable;
   }
+
   public void setMaxNumberOfStudents(int maxNumberOfStudents)
   {
     this.maxNumberOfStudents = maxNumberOfStudents;
+  }
+
+  public void setNumberOfChairs(int numberOfChairs)
+  {
+    this.numberOfChairs = numberOfChairs;
+  }
+
+  public void setNumberOfTables(int numberOfTables)
+  {
+    this.numberOfTables = numberOfTables;
+  }
+
+  public void setCanBeMerged(boolean canBeMerged)
+  {
+    this.canBeMerged = canBeMerged;
   }
 
   public boolean equals(Object obj)
@@ -49,9 +88,13 @@ public class Room implements Serializable
     if (!(obj instanceof Room))
       return false;
     Room other = (Room) obj;
-    return other.roomName.equals(this.roomName)
-        && other.hasProjector == this.hasProjector
-        && other.maxNumberOfStudents == this.maxNumberOfStudents;
+    return (other.roomName.equals(this.roomName)
+        && other.typesOfConnectorsAvailable
+        .equals(this.typesOfConnectorsAvailable)
+        && other.maxNumberOfStudents == this.maxNumberOfStudents
+        && other.numberOfTables == this.numberOfTables
+        && other.numberOfChairs == this.numberOfChairs
+        && other.canBeMerged == this.canBeMerged);
   }
 
   public String toString()
