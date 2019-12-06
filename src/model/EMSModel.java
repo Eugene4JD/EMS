113594f3e768cccd49f3d6adb98c22ce38fffac1
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2019-12-06 11:47:13 +0100

    11 47

--- src/model/EMSModel.java
+++ src/model/EMSModel.java
@@ -2,10 +2,12 @@ package model;
 
 public interface EMSModel
 {
-   void addStudent (String name, int id, int Semester);
-   void addTeacher (String name, int id, String subject);
-   void addRoom (String roomName, boolean isThereProjector, int maxNumberOfStudents);
-   void addExam(Exam exam);
-   void addClass(String ClassName, TeacherList Teachers, StudentList Students);
-   void removeExam(DateAndTimePeriod period);
+  void addStudent(String name, int id, int Semester);
+  void addTeacher(String name, int id, String subject);
+  void addRoom(String roomName, String typesOfConnectorsAvailable,
+      int maxNumberOfStudents, int numberOfChairs, int numberOfTables,
+      boolean canBeMerged);
+  void addExam(Exam exam);
+  void addClass(String ClassName, TeacherList Teachers, StudentList Students);
+  void removeExam(DateAndTimePeriod period);
 }
--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,23 +1,27 @@
 package view;
+
 import model.EMSModel;
 
 import java.util.Scanner;
+
 public class ConsoleView
 {
   private EMSModel model;
 
-  public ConsoleView(EMSModel model){this.model = model;}
+  public ConsoleView(EMSModel model)
+  {
+    this.model = model;
+  }
+
   public void start()
   {
     Scanner input = new Scanner(System.in);
     boolean running = true;
-
-
     while (running)
     {
-      System.out.println("1) Add Student");
+      System.out.println("1) Add Student"); //done
       System.out.println("2) Add Teacher");
-      System.out.println("3) Add Room");
+      System.out.println("3) Add Room"); //done
       System.out.println("4) Add Class");
       System.out.println("5) Add Exam");
       System.out.println("6) Get grade");
@@ -39,18 +43,38 @@ public class ConsoleView
           int Id = input.nextInt();
           System.out.println("Enter the valid semester:");
           int semester = input.nextInt();
-          model.addStudent(name,Id,semester);
+          model.addStudent(name, Id, semester);
           break;
         case 2:
           break;
         case 3:
+          System.out.println("roomName?");
+          String roomName = input.nextLine();
+          System.out.println("connectorsAvailable?");
+          String typesOfConnectorsAvailable = input.nextLine();
+          System.out.println("maxNumberOfStudents?");
+          int maxNumberOfStudents = input.nextInt();
+          System.out.println("numberOfChairs?");
+          int numberOfChairs = input.nextInt();
+          System.out.println("numberOfTables?");
+          int numberOfTables = input.nextInt();
+          System.out.println("canBeMerged? (0 or 1)");
+          int buffer = input.nextInt();
+          boolean canBeMerged;
+          if (buffer == 0)
+            canBeMerged = false;
+          else
+            canBeMerged = true;
+          model.addRoom(roomName, typesOfConnectorsAvailable,
+              maxNumberOfStudents, numberOfChairs, numberOfTables, canBeMerged);
           break;
         case 4:
           break;
         case 0:
           running = false;
           break;
-        default: System.out.println("The wrong input, please re enter the value!");
+        default:
+          System.out.println("The wrong input, please re enter the value!");
       }
     }
 
