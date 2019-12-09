b6f9bd796c4cffc1d793b106f163f14b3ff092ef
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2019-12-09 13:57:29 +0100

    13 57

--- src/model/Exam.java
+++ src/model/Exam.java
@@ -4,14 +4,15 @@ public class Exam
 {
   private String examName;
   private ClassList examClasses;
-  private DateAndTimePeriod periodOExam;
+  private DateAndTimePeriod periodOfExam;
   private RoomList examRooms;
+  private String description;
 
-  public Exam(String examName, Date  DateOfTheExam, Time startTime, Time endTime)
+  public Exam(String examName, Date dateOfTheExam, Time startTime, Time endTime)
   {
     this.examName = examName;
     examClasses = new ClassList();
-    periodOExam = new DateAndTimePeriod(DateOfTheExam, startTime, endTime);
+    periodOfExam = new DateAndTimePeriod(dateOfTheExam, startTime, endTime);
     examRooms = new RoomList();
   }
 
@@ -19,40 +20,47 @@ public class Exam
   {
     this.examClasses = examClasses;
   }
+
   public void addClass(Class classItem)
   {
     examClasses.addClass(classItem);
   }
+
   public void removeClassByClassName(String className)
   {
     examClasses.removeClassByClassName(className);
   }
+
   public void removeClassByIndex(int index)
   {
     examClasses.removeClassByIndex(index);
   }
+
   public void removeAllClasses()
   {
     examClasses.removeAll();
   }
 
-
   public void addRooms(RoomList roomList)
   {
     examRooms = roomList;
   }
+
   public void addRoom(Room room)
   {
     examRooms.addRoom(room);
   }
+
   public void removeRoomByRoomName(String roomName)
   {
     examRooms.removeRoomByRoomName(roomName);
   }
+
   public void removeRoomByIndex(int index)
   {
     examRooms.removeRoom(index);
   }
+
   public void removeAllRooms()
   {
     examRooms.removeAll();
@@ -62,34 +70,34 @@ public class Exam
   {
     String str = "";
     if (examClasses.getNumberOfClasses() > 0)
-      str+=examClasses.getClassByIndex(0).getClassName();
-    for (int i = 1; i< examClasses.getNumberOfClasses(); i++)
+      str += examClasses.getClassByIndex(0).getClassName();
+    for (int i = 1; i < examClasses.getNumberOfClasses(); i++)
     {
-      str+="-"+examClasses.getClassByIndex(i);
+      str += "-" + examClasses.getClassByIndex(i);
     }
     return str;
   }
 
   public boolean equals(Object object)
   {
-    if (!(object instanceof Exam ))
+    if (!(object instanceof Exam))
       return false;
     Exam other = (Exam) object;
-    return other.examClasses.equals(this.examClasses)
-        && other.examRooms.equals(this.examRooms)
-        && other.periodOExam.equals(this.periodOExam);
+    return other.examClasses.equals(this.examClasses) && other.examRooms
+        .equals(this.examRooms) && other.periodOfExam.equals(this.periodOfExam);
   }
 
   public DateAndTimePeriod getPeriodOExam()
   {
-    return this.periodOExam;
+    return this.periodOfExam;
   }
+
   public String showScheduleElement()
   {
     String str = "";
-    str+= this.examName+" "+this.examRooms + " " + this.periodOExam.toString();
+    str += this.examName + " " + this.examRooms + " " + this.periodOfExam
+        .toString();
     return str;
   }
-
 
 }
