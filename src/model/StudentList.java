5cf0a32701b11edf4bb99df1b05b7807c9d8ea1a
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2019-12-13 12:07:41 +0100

    1207

--- src/model/StudentList.java
+++ src/model/StudentList.java
@@ -14,7 +14,7 @@ public class StudentList implements Serializable
 
   public boolean isStudentExist(Student student)
   {
-    for (int i = 0 ; i< students.size(); i++)
+    for (int i = 0; i < students.size(); i++)
     {
       if (students.get(i).equals(student))
       {
@@ -23,55 +23,66 @@ public class StudentList implements Serializable
     }
     return false;
   }
+
   public void addStudent(Student student)
   {
     if (!(isStudentExist(student)))
       students.add(student);
   }
+
   public int getIndexOfStudentById(int id)
   {
-    for (int i = 0 ; i<students.size();i++)
+    for (int i = 0; i < students.size(); i++)
     {
       if (students.get(i).getId() == id)
         return i;
     }
     return -1;
   }
-  public void removeStudentByIndex (int index)
+
+  public void removeStudentByIndex(int index)
   {
     students.remove(index);
   }
+
   public int size()
   {
     return students.size();
   }
-  public void removeStudentById (int id)
+
+  public void removeStudentById(int id)
   {
     if (getIndexOfStudentById(id) != -1)
     {
       students.remove(getIndexOfStudentById(id));
     }
   }
+
   public Student getStudentById(int id)
   {
     return students.get(getIndexOfStudentById(id));
   }
+
   public void removeStudentByObject(Student student)
   {
-    students.remove(student);
+      students.remove(student);
   }
+
   public Student getStudentByIndex(int index)
   {
     return students.get(index);
   }
+
   public int getNumberOfStudents()
   {
-    return  students.size();
+    return students.size();
   }
+
   public ArrayList<Student> getStudents()
   {
     return students;
   }
+
   public boolean equals(Object object)
   {
     if (!(object instanceof StudentList))
@@ -79,19 +90,20 @@ public class StudentList implements Serializable
     StudentList other = (StudentList) object;
     if (other.getNumberOfStudents() != this.getNumberOfStudents())
       return false;
-    for (int i = 0; i<this.getNumberOfStudents(); i++)
+    for (int i = 0; i < this.getNumberOfStudents(); i++)
     {
       if (!(this.getStudentByIndex(i)).equals(other.getStudentByIndex(i)))
         return false;
     }
     return true;
   }
+
   public String showAllStudents()
   {
     String str = "";
-    for (int i = 0; i< students.size(); i++)
+    for (int i = 0; i < students.size(); i++)
     {
-      str += students.get(i).toString()+"\n";
+      str += students.get(i).toString() + "\n";
     }
     return str;
   }
